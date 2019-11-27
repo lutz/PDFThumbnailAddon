d87023c1f99fa799fb419134d2306e696ad73c33
Daniel Lutz <daniel.lutz@outlook.com>
2019-11-27 19:39:00 +0100

    code init

---
+++ src/PDFThumbnailAddon/Addon.cs
@@ -0,0 +1,105 @@
+﻿using SwissAcademic.Citavi.Controls.Wpf;
+using SwissAcademic.Citavi.Shell;
+using System;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+using System.Linq;
+
+namespace PDFThumbnailAddon
+{
+    public class Addon : CitaviAddOn<MainForm>
+    {
+        #region Methods
+
+        public override void OnHostingFormLoaded(MainForm mainForm)
+        {
+            AddTabPageToSideBar(mainForm);
+
+            Observe(mainForm, true);
+
+            base.OnHostingFormLoaded(mainForm);
+        }
+
+        void Observe(MainForm mainForm, bool observe)
+        {
+            if (observe)
+            {
+                mainForm.FormClosed += MainForm_FormClosed;
+                
+                var viewer = mainForm.PreviewControl.GetPdfViewControl();
+                viewer.DocumentChanged += Viewer_DocumentChanged;
+                viewer.DocumentClosing += Viewer_DocumentClosing;
+            }
+            else
+            {
+                mainForm.FormClosed -= MainForm_FormClosed;
+
+                var viewer = mainForm.PreviewControl.GetPdfViewControl();
+                viewer.DocumentChanged -= Viewer_DocumentChanged;
+                viewer.DocumentClosing -= Viewer_DocumentClosing;
+            }
+        }
+
+        void AddTabPageToSideBar(MainForm mainForm)
+        {
+            if (mainForm.PreviewControl.GetPdfViewControl().GetSideBar() is System.Windows.Controls.TabControl tabControl)
+            {
+             
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.UriSource = new Uri("/PDFThumbnailAddon;component/Resources/thumbs.png", UriKind.RelativeOrAbsolute);
+                bitmapImage.EndInit();
+
+                var thumbnailControl = new ThumbnailsControl(mainForm.GetViewer());
+
+                var tabPage = new TabItem
+                {
+                    Tag="Addon",
+                    Header = new Image { Height = 16, Source = bitmapImage },
+                    Content = thumbnailControl
+                };
+
+                tabControl.Items.Add(tabPage);
+
+                thumbnailControl.Refresh();
+            }
+        }
+
+        #endregion
+
+        #region EventHandlers
+
+        private void Viewer_DocumentChanged(object sender, EventArgs e)
+        {
+            if (sender is PdfViewControl viewer && viewer.GetSideBar() is System.Windows.Controls.TabControl tabControl)
+            {
+                var tabitem = tabControl.Items.Cast<TabItem>().FirstOrDefault(item => item.Tag != null && item.Tag.ToString().Equals("Addon", StringComparison.OrdinalIgnoreCase));
+                if (tabitem != null && tabitem.Content is ThumbnailsControl thumbnailsControl)
+                    thumbnailsControl.Refresh();
+            }
+
+            
+        }
+
+        private void Viewer_DocumentClosing(object sender, DocumentClosingArgs args)
+        {
+            if (sender is PdfViewControl viewer && viewer.GetSideBar() is System.Windows.Controls.TabControl tabControl)
+            {
+                var tabitem = tabControl.Items.Cast<TabItem>().FirstOrDefault(item => item.Tag != null && item.Tag.ToString().Equals("Addon", StringComparison.OrdinalIgnoreCase));
+                if (tabitem != null && tabitem.Content is ThumbnailsControl thumbnailsControl)
+                    thumbnailsControl.Clear();
+            }
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (sender is MainForm mainForm)
+            {
+                Observe(mainForm, false);
+            }
+        }
+
+        #endregion
+    }
+}
